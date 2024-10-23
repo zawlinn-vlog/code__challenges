75cfe8837e141dee482b3ac13a1c5843c5387b87
User <user@Users-MacBook-Pro.local>
2024-10-23 10:49:51 +0700

    Challenge: Add Challenge 17 keeping working

---
+++ 17 Challenge/assets/js/app.js
@@ -0,0 +1,31 @@
+// Fixed Flex Gap no Support Browser
+
+function checkFlexGap() {
+  // CREATE DIV ELEMENT
+  let flex = document.createElement("div");
+
+  // APPEND STYLE
+  flex.style.display = "flex";
+  flex.style.flexDirection = "Column";
+  flex.style.rowGap = "1px";
+
+  // APPEND TO DOCUMENT
+  document.body.appendChild(flex);
+
+  // CREATE TWO CHILD ELEMENT
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  // CHECK GAP IS SUPPORT OR NOT
+  let isSupported = flex.scrollHeight === 1;
+
+  // REMOVE FLEX ELEMENT
+  flex.parentNode.removeChild(flex);
+
+  console.log(isSupported);
+
+  // ADD CLASS TO DOCUMENT BODY
+  if (!isSupported) document.body.classList.add("no--flex-gap");
+}
+
+checkFlexGap();
